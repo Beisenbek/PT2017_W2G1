a8306e41715642e3e3232c74c19ad496a6213626
Beisenbek <beysenbek@gmail.com>
2017-01-23 14:46:45 +0600

    Lab2

---
+++ Example1/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Animal
+    {
+        int age;
+        public string name;
+        public double mass;
+        public bool hungry;
+        public Animal()
+        {
+            age = 0;
+            mass = 0;
+            hungry = true;
+            name = "animal";
+        }
+
+        public void SetAge(int _age)
+        {
+            if (_age < 0) return;
+            if (_age > 100) return;
+            age = _age;
+        }
+        public int GetAge(string userName)
+        {
+            if (userName == "owner")
+            {
+                return age;
+            }
+            return -1;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+    }
+
+    class BankAccount
+    {
+        public BankAccount()
+        {
+            accountInfo = "";
+        }
+        string accountInfo;
+        public string AccountInfo
+        {
+            get
+            {
+                return accountInfo;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    accountInfo = value;
+                }
+            }
+        }
+    }
+
+    class Rectangle
+    {
+        private double square;
+        private double h;
+        private double w;
+
+        public Rectangle(double h, double w)
+        {
+            this.w = w;
+            this.h = h;
+            square = w * h;
+        }
+        public Rectangle()
+        {
+
+        }
+        public double Square
+        {
+            get
+            {
+                return square;
+            }
+        }
+        public double W
+        {
+            get
+            {
+                return w;
+            }
+            set
+            {
+                w = value;
+                square = h * w;
+            }
+        }
+        public double H
+        {
+            get
+            {
+                return h;
+            }
+            set
+            {
+                h = value;
+                square = h * w;
+            }
+        }
+
+        public static Rectangle operator +(Rectangle r1, Rectangle r2)
+        {
+            Rectangle r = new Rectangle();
+            r.H = r1.H + r2.H;
+            r.W = r1.W + r2.W;
+            return r;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F3();
+        }
+
+        private static void F3()
+        {
+            Rectangle r = new Rectangle(5, 6);
+            Rectangle r2 = new Rectangle(2, 6);
+            Rectangle r3 = r + r2;
+            int x = 2;
+            int y = 3;
+            Console.WriteLine(x + y);
+
+            Console.WriteLine(r3.Square);
+        }
+
+        public static void F2()
+        {
+            BankAccount b = new BankAccount();
+            b.AccountInfo = "User1 2000$";
+            Console.WriteLine(b.AccountInfo);
+            BankAccount c = new BankAccount { AccountInfo = "User2 3000$" };
+            Console.WriteLine(c.AccountInfo);
+        }
+
+        private static void F1()
+        {
+            Animal a = new Animal();
+            a.SetAge(10);
+            a.Age = 20;
+            Console.WriteLine(a.GetAge("owner"));
+            Console.WriteLine(a.Age);
+        }
+
+
+    }
+}
---
+++ Example3/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F6();
+        }
+
+        private static void F6()
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("test");
+        }
+
+        private static void F5()
+        {
+            bool alive = true;
+            while (alive)
+            {
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.A:
+                        Console.WriteLine("YES!");
+                        break;
+                    case ConsoleKey.Escape:
+                        Console.WriteLine("Bye!");
+                        alive = false;
+                        break;
+                    default:
+                        Console.WriteLine("NO!");
+                        break;
+                }
+               
+            }
+        }
+
+        private static void F4()
+        {
+            while (true)
+            {
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                if (pressedKey.Key == ConsoleKey.A)
+                {
+                    Console.WriteLine("YES!");
+                }
+                else if (pressedKey.Key == ConsoleKey.Escape)
+                {
+                    Console.WriteLine("Bye!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("NO!");
+                }
+            }
+        }
+        static void ShowDirectoryContent(string path)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            /*DirectoryInfo[] x = di.GetDirectories();
+              var y = di.GetDirectories();
+            /*for (int i = 0; i < x.Length; ++i)
+            {
+                Console.WriteLine(x[i].Name);
+            }*/
+            foreach (DirectoryInfo d in di.GetDirectories())
+            {
+                Console.WriteLine(d.Name);
+            }
+
+            foreach (FileInfo f in di.GetFiles())
+            {
+                Console.WriteLine(f.Name);
+            }
+            
+        }
+        private static void F1()
+        {
+            ShowDirectoryContent(@"C:\QtSDK\Desktop\Qt\4.8.0\mingw");
+        }
+
+        private static void F2()
+        {
+            FileStream fs = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+
+            sw.WriteLine("Hello world!");
+
+            sw.Close();
+            fs.Close();
+        }
+
+        private static void F3()
+        {
+            FileStream fs = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs);
+
+            string text = sr.ReadToEnd();
+
+            Console.WriteLine(text);
+
+            sr.Close();
+            fs.Close();
+        }
+
+
+
+    }
+}
